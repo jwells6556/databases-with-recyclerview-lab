0395b18b7fb8efff854831b4a0e63fbdbfe59aca
jwells6556 <jwells6556@gmail.com>
2016-10-25 17:02:37 -0400

    not really working will troubleshoot

--- starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
@@ -4,13 +4,21 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.List;
+
+import ly.generalassemb.drewmahrt.shoppinglistver2.setup.CustomRecyclerViewAdapter;
+import ly.generalassemb.drewmahrt.shoppinglistver2.setup.CustomSQLiteOpenHelper;
 import ly.generalassemb.drewmahrt.shoppinglistver2.setup.DBAssetHelper;
+import ly.generalassemb.drewmahrt.shoppinglistver2.setup.ListItem;
+import ly.generalassemb.drewmahrt.shoppinglistver2.setup.CustomSQLiteOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -22,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
 
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        List<ListItem>shoppingList = CustomSQLiteOpenHelper.getInstance(MainActivity.this)
+                .getAllAsList();
+
+
+        recyclerView.setAdapter(new CustomRecyclerViewAdapter(shoppingList));
     }
 }
